f2ebd2db7f0b26b7b6a03d550bb90b5c3ed0da18
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-08-16 20:41:52 -0300

    projeto-barbearia

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+      <div id="nav">
+
+          <router-link to="/" >
+              <img :src="logo" :alt="alt" id="logo">
+          </router-link>
+        
+    <router-link to="/">Home</router-link> |
+    <router-link to="/agendamento">Agendamento</router-link> |
+    <router-link to="/teste" > Teste </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+    props: ["logo", "alt"]
+    
+}
+</script>
+
+
+<style scoped>
+     h1 {
+    color: red;
+  }
+</style>
---
+++ src/views/Agendamento.vue
@@ -0,0 +1,5 @@
+<template>
+  <div >
+    <h1>AGENDAMENTO</h1>
+  </div>
+</template>
